07bfd53970b6f7b52d54ad4638ba540f0159e552
silverbullet11 <gongming119@hotmail.com>
2018-04-30 20:34:39 +0800

    Update run.py

--- research/run.py
+++ research/run.py
@@ -5,5 +5,14 @@ w_d = os.path.dirname(os.path.abspath(__file__))
 print('change working dir to [{0}]'.format(w_d))
 os.chdir(w_d)
 
+print('/home/silverbullet11:')
+print(os.listdir('/home/silverbullet11'))
+
+print('/data:')
+print(os.listdir('/data'))
+
+print('/data/silverbullet11:')
+print(os.listdir('/data/silverbullet11'))
+
 for l in os.popen('/bin/bash -c "cd {0} && source ./run.sh"'.format(w_d)):
     print(l.strip())
